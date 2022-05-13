b23e94467986fcae443d35a1d1898d523a87802b
Terry Wachuka <79338912+T-erry@users.noreply.github.com>
2022-05-13 15:19:21 +0300

    added current description icon of a city

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function formatDate(timestamp) {
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = `0${hours}`;
+    minutes = `0${minutes}`;
   }
   let days = [
     "Sunday",
@@ -21,6 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 function displayTemperature(response) {
+  console.log(response);
+  let iconElement = document.querySelector("#icon");
   let dateElement = document.querySelector("#date");
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
@@ -34,8 +36,13 @@ function displayTemperature(response) {
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", `response.data.weather[0].description`);
 }
 let apiKey = "0d6137aeec57493e132a26d1fb505d97";
-let city = "Berlin";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
